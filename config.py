dab72b31fe55d8ff712785dba4a3dc13b0b4b263
lirika <139256064+li6ri9ka@users.noreply.github.com>
2024-06-14 12:16:23 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+file = [('/Users/Li6ri9ka/PycharmProjects/practce/access.log', ['%h', '%t', '%r', '%>s', '%b'])]
+db_info = {
+            'database': 'practice',
+            'user': 'postgres',
+            'password': 'Cotlin1r253107',
+            'host': '127.0.0.1',
+            'port': '5432'
+}
